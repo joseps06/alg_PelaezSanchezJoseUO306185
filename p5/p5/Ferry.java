92c3590dece0ce66f574ceafb2d899c06bcb6bf7
José Peláez Sánchez <UO306185@uniovi.es>
2026-03-18 19:55:11 +0100

    Algoritmia_Ferry

---
+++ p5/p5/Ferry.java
@@ -0,0 +1,76 @@
+package p5;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ferry
+{
+    private int boatLength; //Longitud de los carriles del barco
+    private List<Integer> vehicles; //Lista de vehiculos
+    private boolean[][] dp; //Matriz con las posibles soluciones
+    private int[] sumatorio; //Suma acumulada de las longitudes de los vehiculos
+
+    public Ferry(int length, List<Integer> vehicles) {
+        this.boatLength = length;
+        this.vehicles = vehicles;
+        this.dp = new boolean[vehicles.size()+1][boatLength+1];
+
+        this.sumatorio = new int[vehicles.size() + 1];
+
+        //Inicializamos sumatorio
+        this.sumatorio[0] = 0;
+        for (int i=1; i<=vehicles.size(); i++) {
+            this.sumatorio[i] = sumatorio[i-1] + vehicles.get(i-1);
+        }
+
+
+    }
+
+    public void run() {
+        dp[0][0] = true;
+        for (int  i=1; i<=vehicles.size(); i++) {
+            for (int l=boatLength; l>=0; l--) {
+                if (!dp[i-1][l]) {
+                    continue;  //Si el coche anterior no entra no tiene sentido seguir con el bucle
+                }
+
+                //meter coche en babor
+                if (l+vehicles.get(i)<=boatLength) {
+                    dp[i][vehicles.get(i)-1] = true;
+                }
+                //meter coche en estribor
+                if ((sumatorio[i]-l)<=boatLength) {
+                    dp[i][l] = true;
+                }
+            }
+        }
+    }
+
+    public void printData() {
+        System.out.printf("Longitud de los carriles: %d", boatLength);
+        System.out.println("Longitud de los vehiculos:\n");
+        for (int i=0; i<vehicles.size(); i++) {
+            System.out.printf("\tVehiculo %d: %d unidades\n", i+1, vehicles.get(i));
+        }
+    }
+
+    private void loadData (String file) {
+        this.vehicles = new ArrayList<Integer>();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            this.boatLength = Integer.valueOf(reader.readLine());
+            for (String s : reader.readLine().split(" ")) {
+                this.vehicles.add(Integer.valueOf(s));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }                         
+}
